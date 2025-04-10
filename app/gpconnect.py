50fec17baa1f84e00a41e2d15634306c6791b992
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2025-04-10 11:16:20 +0100

    adding in more logging and script to test open test patients

--- app/gpconnect.py
+++ app/gpconnect.py
@@ -17,6 +17,44 @@ from .security import create_jwt
 
 router = APIRouter()
 
+logger = logging.getLogger("httpx_logger")
+logger.setLevel(logging.INFO)
+# Create file handler to write logs to a file
+file_handler = logging.FileHandler("httpx_logs.log")
+file_handler.setLevel(logging.INFO)
+
+# Create formatter and add it to the handler
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(file_handler)
+
+
+async def log_request(request: httpx.Request):
+    logger.info("Outgoing Request:")
+    logger.info(f"{request.method} {request.url}")
+    logger.info(f"Headers: {dict(request.headers)}")
+    if request.content:
+        try:
+            logger.info(f"Body: {request.content.decode('utf-8')}")
+        except UnicodeDecodeError:
+            logger.info("Body: [Binary Content]")
+    logger.info("-----")
+
+
+async def log_response(response: httpx.Response):
+    content_bytes = await response.aread()
+    try:
+        content_str = content_bytes.decode("utf-8")
+    except UnicodeDecodeError:
+        content_str = "[Binary Content]"
+    logger.info("Incoming Response:")
+    logger.info(f"Status Code: {response.status_code}")
+    logger.info(f"Headers: {dict(response.headers)}")
+    logger.info(f"Body: {content_str}")
+    logger.info("=====")
+
 
 @router.get("/gpconnect/{nhsno}")
 async def gpconnect(nhsno: int):
@@ -73,8 +111,16 @@ async def gpconnect(nhsno: int):
         json=body,
         headers=headers,
     )
-    print(r.text)
-    logging.info(r.text)
+    # log the request and response
+    async with httpx.AsyncClient(event_hooks={
+        "request": [log_request],
+        "response": [log_response]
+    }) as client:
+        r = await client.post(
+            "https://orange.testlab.nhs.uk/B82617/STU3/1/gpconnect/structured/fhir/Patient/$gpc.getstructuredrecord",
+            json=body,
+            headers=headers,
+        )
 
     scr_bundle = json.loads(r.text)
 
