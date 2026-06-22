28793e51178fe062f81283d97b3b5a416cf494fd
OliverSchafer <oliver.schafer@nhs.net>
2026-06-22 16:48:39 +0100

    fix: set require_mtls to true

--- infra/variables.tf
+++ infra/variables.tf
@@ -163,9 +163,9 @@ variable "allowed_hosts" {
 }
 
 variable "require_mtls" {
-  description = "Enforce mTLS validation in the application"
+  description = "Whether to enforce mTLS globally in the FastAPI app"
   type        = string
-  default     = "false"
+  default     = "true"
 }
 
 variable "external_relay_url" {
