468d43451bf09f35ded9e0a12a9c7b17a1b26d0e
OliverSchafer <oliver.schafer@nhs.net>
2026-08-26 14:03:26 +0100

    fix: remediate XXE vulnerability by replacing insecure xml.etree with defusedxml (CWE-611)

--- app/ccda/helpers.py
+++ app/ccda/helpers.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List
-from xml.etree import ElementTree
+from defusedxml import ElementTree
 
 import xmltodict
 from fhirclient.models import coding, organization, period
