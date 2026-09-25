48a72b77564048340d94b3e1dc2d3efb4ed278f6
OliverSchafer <oliver.schafer@nhs.net>
2026-09-25 11:59:06 +0100

    infra: preserve soft-deleted key vaults during rehearsal teardown

--- infra/versions.tf
+++ infra/versions.tf
@@ -9,7 +9,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
 provider "azurerm" {
